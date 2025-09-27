4e758cda737c5ea43d6b9ed437d7eb41eebea40e
tgzgondoz <tgzgondozz@gmail.com>
2025-09-27 21:15:41 +0200

    My rentals

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import CarList from './components/CarList';
 import Footer from './components/Footer';
 import AdminLogin from './components/AdminLogin';
 import AdminPanel from './components/AdminPanel';
+import RentalForm from './components/RentalForm';
+import RentalConfirmation from './components/RentalConfirmation';
 import { useState } from 'react';
 import './App.css';
 
@@ -15,7 +17,10 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
-            <Route path="/" element={<PublicLayout />} />
+            {/* Public Routes with Header & Footer */}
+            <Route path="/*" element={<PublicLayout />} />
+            
+            {/* Admin Routes */}
             <Route path="/admin/login" element={<AdminLogin />} />
             <Route path="/admin" element={<ProtectedRoute><AdminPanel /></ProtectedRoute>} />
           </Routes>
@@ -32,8 +37,22 @@ function PublicLayout() {
     <>
       <Header />
       <main>
-        <Hero onSearch={setSearchTerm} />
-        <CarList searchTerm={searchTerm} />
+        <Routes>
+          {/* Homepage with Hero and CarList */}
+          <Route path="/" element={
+            <>
+              <Hero onSearch={setSearchTerm} />
+              <CarList searchTerm={searchTerm} />
+            </>
+          } />
+          
+          {/* Rental Process Routes */}
+          <Route path="/rent-car/:carId" element={<RentalForm />} />
+          <Route path="/rental-confirmation/:carId" element={<RentalConfirmation />} />
+          
+          {/* Catch all route - redirect to home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </main>
       <Footer />
     </>
