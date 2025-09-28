362bc7073a372aa956c4f5ca8e26d7a56bfc73ef
tgzgondoz <tgzgondozz@gmail.com>
2025-09-28 08:48:12 +0200

    Admin panel UI

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,13 @@
+// src/firebase/config.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdwEeVAvHU6cUxHCaIfcGY1VHXjUW6fow",
   authDomain: "carrentalapp-b6862.firebaseapp.com",
+  databaseURL: "https://carrentalapp-b6862-default-rtdb.firebaseio.com", // Add this
   projectId: "carrentalapp-b6862",
   storageBucket: "carrentalapp-b6862.firebasestorage.app",
   messagingSenderId: "722148486629",
@@ -12,8 +15,8 @@ const firebaseConfig = {
   measurementId: "G-NKT1P56X3K"
 };
 
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const storage = getStorage(app);
 export default app;
