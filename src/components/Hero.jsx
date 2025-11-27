a933b07dd7d0e577baf0f7a009c7d42e15154dfe
tgzgondoz <tgzgondozz@gmail.com>
2025-11-27 18:04:29 +0200

    All files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,6 @@ const Hero = ({ onSearch }) => {
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
             backgroundImage: 'url("https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2000&q=80")'
-            
           }}
         />
         
